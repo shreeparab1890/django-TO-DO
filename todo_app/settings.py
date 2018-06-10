a7e45cf71656b916cc631113915d6135018497f7
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2018-06-10 13:19:55 +0530

    Update settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,5 +129,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT  = 'static/'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+#db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
